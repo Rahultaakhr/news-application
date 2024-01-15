1c5632aeb69858b8cb9852082415fe4801f9960e
Rahultaakhr <148430988+Rahultaakhr@users.noreply.github.com>
2024-01-15 06:25:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-
 const ApiKey = "8c54fdb693cd4e28acfe309ac929ba83"
 const url = "https://newsapi.org/v2/everything?q="
-const searchBtn= document.getElementById('search-button')
-const searchInput=document.getElementById('search-input')
+const searchBtn = document.getElementById('search-button')
+const searchInput = document.getElementById('search-input')
 
 
 // searchBtn.addEventListener('click',fetchNews(`${searchInput.value}`))
@@ -16,24 +15,21 @@ async function fetchNews(query) {
     console.log(data);
     bindData(data.articles);
 }
-function bindData(articles) {
+
+function bindData(articles = []) {
     const cardContainer = document.getElementById('card-container');
     const newsCardTemplate = document.getElementById('template-news-card');
 
     cardContainer.innerHTML = '';
 
-    // Check if articles is defined and is an array
-    if (Array.isArray(articles)) {
-        articles.forEach((article) => {
-            if (!article.urlToImage) return;
-            const cardClone = newsCardTemplate.content.cloneNode(true);
-            fillDataCard(cardClone, article);
-            cardContainer.appendChild(cardClone);
-        });
-    } else {
-        console.error('Invalid or undefined articles data.');
-    }
+    articles.forEach((article) => {
+        if (article && !article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataCard(cardClone, article);
+        cardContainer.appendChild(cardClone);
+    });
 }
+
 
 function fillDataCard(cardClone, article) {
     const cardImg = cardClone.querySelector('#news-img')
@@ -47,31 +43,31 @@ function fillDataCard(cardClone, article) {
         timeZone: "Asia/Jakarta"
     })
     newsSource.innerHTML = `${article.source.name} - ${date}`;
-    cardClone.firstElementChild.addEventListener('click',()=>{
-window.open(article.url,"_blank")
+    cardClone.firstElementChild.addEventListener('click', () => {
+        window.open(article.url, "_blank")
     })
 }
-let CurSelectedNav= null;
+let CurSelectedNav = null;
 
 
-function clickFun(id){
+function clickFun(id) {
     fetchNews(id)
-    const navItem=document.getElementById(id)
+    const navItem = document.getElementById(id)
     CurSelectedNav?.classList.remove('active')
-    CurSelectedNav=navItem;
+    CurSelectedNav = navItem;
     CurSelectedNav.classList.add('active')
 }
-searchBtn.addEventListener('click',()=>{
-    const query =searchInput.value;
-    if(!query) return;
+searchBtn.addEventListener('click', () => {
+    const query = searchInput.value;
+    if (!query) return;
     fetchNews(query)
     console.log(query);
     CurSelectedNav?.classList.remove('active')
-    CurSelectedNav= null;
-    
-})
- 
+    CurSelectedNav = null;
 
-function reload(){
+})
+
+
+function reload() {
     window.location.reload()
 }
